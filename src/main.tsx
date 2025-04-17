94db6456e361ca9c9d6fbba5b69e082bba7eee76
adityavardhansharma <adityaradhika13@gmail.com>
2025-04-17 15:54:24 +0530

    updated ui

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { ClerkProvider } from "@clerk/clerk-react";
 import "./index.css";
 import App from "./App.tsx";
 import { ToasterProvider } from "./provider/toast-provider.tsx";
+import { clerkAppearance } from "./config/clerk-appearance.ts";
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -16,7 +17,11 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    <ClerkProvider 
+      publishableKey={PUBLISHABLE_KEY} 
+      afterSignOutUrl="/"
+      appearance={clerkAppearance}
+    >
       <App />
       <ToasterProvider />
     </ClerkProvider>
