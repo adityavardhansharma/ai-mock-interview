e8b92f6589d892c7ff590713e068df3f01d7c0de
adityavardhansharma <adityaradhika13@gmail.com>
2025-04-17 15:54:04 +0530

    updated ui

--- src/layouts/auth-layout.tsx
+++ src/layouts/auth-layout.tsx
@@ -2,12 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const AuthenticationLayout = () => {
   return (
-    <div className="w-screen h-screen overflow-hidden flex items-center justify-center relative">
-      <img
-        src="/assets/img/bg.png"
-        className="absolute w-full h-full object-cover opacity-20"
-        alt=""
-      />
+    <div className="w-screen h-screen overflow-hidden flex items-center justify-center">
       <Outlet />
     </div>
   );
